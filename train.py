d2d90d2cb4a07406dbb76d8ca9375dac35b1da85
Mufumi Rikhotso <36229418+Mufumi@users.noreply.github.com>
2021-07-17 15:33:41 +0200

    Update train.py

--- train.py
+++ train.py
@@ -15,16 +15,6 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 url_path ='https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'
 
-ds = TabularDatasetFactory.from_delimited_files(path=url_path)
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
-
-run = Run.get_context()
-
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -71,3 +61,13 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
+ds = TabularDatasetFactory.from_delimited_files(path=url_path)
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
+
+run = Run.get_context()
