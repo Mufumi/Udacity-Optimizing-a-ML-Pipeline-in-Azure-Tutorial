7a8f4b1ed5dbb2e267e8d9b41fbdeac1d8772a93
Mufumi Rikhotso <36229418+Mufumi@users.noreply.github.com>
2021-07-18 18:53:23 +0200

    Update train.py with scaling

--- train.py
+++ train.py
@@ -47,9 +47,14 @@ def clean_data(data):
 
 x, y = clean_data(ds)
 
-
-
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(x_train)
+X_test = sc.transform(x_test)
+
+
 
 run = Run.get_context()
 
