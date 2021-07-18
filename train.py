9cba55ff9aa4279597d23d1dd46abc42d8891d5e
Mufumi Rikhotso <36229418+Mufumi@users.noreply.github.com>
2021-07-18 19:47:50 +0200

    Update train.py to One Hot Encoder

--- train.py
+++ train.py
@@ -47,12 +47,13 @@ def clean_data(data):
 
 x, y = clean_data(ds)
 
+#Implement one-hot encoding
+
+x_enc = OneHotEncoder(drop='first').fit_transform(x)
+y_enc = OneHotEncoder().fit_transform(y)
+
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
 
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-x_train = sc.fit_transform(x_train)
-x_test = sc.transform(x_test)
 
 run = Run.get_context()
 
